3af5a84277a4f43838a2c4671653161ac5e50406
jjewel <jjewe2009@gmail.com>
2020-04-18 21:59:27 +1000

    reordered files and added more state management

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -21,6 +22,21 @@ public class AdventureGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ManageState();
+    }
 
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
+        }
+
+        textComponent.text = state.GetStateStory();
     }
 }
